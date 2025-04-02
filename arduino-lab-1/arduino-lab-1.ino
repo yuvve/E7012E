b0ac8db29bf7fa7edd9ced2ae6929f29e958c72f
Emanuel Nordin <emanuel02nordin@gmail.com>
2025-04-02 17:04:26 +0200

    message

--- arduino-lab-1/arduino-lab-1.ino
+++ arduino-lab-1/arduino-lab-1.ino
@@ -1,11 +1,35 @@
+#define F_TO_P_MILLI(f) 1000.0/f
+
+int counter = 0;
+float t = 0.0;
+float t0 = 0.0;
+float f = 1.0;
+
 void setup() {
   pinMode(13, OUTPUT); // Set pin 13 as output
+  pinMode(2, INPUT);
+  attachInterrupt(digitalPinToInterrupt(2), countingISR, RISING);
+  Serial.begin(9600);
 }
 
 void loop() {
   digitalWrite(13, HIGH); // Turn the LED on
-  delay(500);            // Wait one second
+  delay(F_TO_P_MILLI(f)/2);            // Wait one second
   digitalWrite(13, LOW);  // Turn the LED off
-  delay(500);            // Wait one second
-  
+  delay(F_TO_P_MILLI(f)/2);            // Wait one second 
+  if (Serial.available()) {
+    f = Serial.readStringUntil('\n').toFloat();
+  }
+  Serial.print("Counter: ");
+  Serial.println(counter);
+  Serial.print("Periodtid: ");
+  Serial.println((t-t0)/1000000.0);
+  Serial.print("Frekvens: ");
+  Serial.println(f);
+}
+
+void countingISR() {
+  counter ++;
+  t0 = t;
+  t = micros();
 }
