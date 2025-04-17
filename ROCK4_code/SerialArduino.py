3b5a794568d210ba625512991c4a3dd30de23dfb
Calle <callerautio@hotmail.se>
2025-04-17 11:28:19 +0000

    Working basic send recieve

---
+++ ROCK4_code/SerialArduino.py
@@ -0,0 +1,31 @@
+import serial
+import io
+import time
+import threading
+
+ser = serial.Serial("/dev/ttyS2", 115200, timeout = 0.1)
+
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+
+def ser_send():
+    cmd = str(input(">>>")) + "\n"
+    try:
+        sio.write(cmd)
+        sio.flush()
+    except Exception as e:
+        print(e)
+    serSend()
+
+def main():
+    send_thread = threading.Thread(target=ser_send)
+    send_thread.start()
+    
+    while True:
+        try:
+            output = sio.readline().strip()
+            if output != "":
+                print(output)
+        except Exception as e:
+            print(e)
+        
+main()
