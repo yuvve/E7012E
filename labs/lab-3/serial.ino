89cc48ec93656e8190ef1a66dbaf412dbbd434d4
Simon Larsson <99037566+simlar-0@users.noreply.github.com>
2025-04-17 13:26:44 +0200

    Add serial communication to Rock (#6)
    Co-authored-by: Simon Larsson <99037566+simonlarsson517@users.noreply.github.com>

--- labs/lab-3/serial.ino
+++ labs/lab-3/serial.ino
@@ -1,7 +1,16 @@
-void setupSerial() {
-    Serial.begin(9600);
+void setupSerialUSB(int baud) {
+  Serial.begin(baud);
+  
   if DEBUG {
-      Serial.println("Serial initialized");
+      Serial.println("USB Serial initialized");
+  }
+}
+
+void setupSerialRock(int baud) {
+  Serial1.begin(baud);
+
+  if DEBUG {
+    Serial.println("Serial to Rock initialized");
   }
 }
 
@@ -9,4 +18,66 @@ Command parseSerialInput(String input) {
   char cmd = input.charAt(0);
   int value = input.substring(1).toInt();
   return Command {cmd, value};
+}
+
+void sendFeedback() {
+  float current_speed = getSpeed();
+  Serial1.print("FB ");
+  Serial1.print(current_speed);
+  Serial1.print(" ");
+  Serial1.print(targetSpeed);
+  Serial1.print(" ");
+  Serial1.println(targetAngle);
+  Serial1.flush();
+
+  if DEBUG {
+    Serial.print("FB ");
+    Serial.print(current_speed);
+    Serial.print(" ");
+    Serial.print(targetSpeed);
+    Serial.print(" ");
+    Serial.println(targetAngle);
+    Serial.flush();
+  }
+}
+
+void processSerial(Stream& serialPort) {
+  static String input;
+
+  while (serialPort.available()) {
+    char c = serialPort.read();
+    if (c == '\n') {
+      input.trim();
+      Command cmd = parseSerialInput(input);
+
+      switch (cmd.cmd) {
+        case 'M':
+          targetSpeed = cmd.value;
+          sendFeedback();
+
+          if (DEBUG) {
+            Serial.print(cmd.cmd);
+            Serial.println(cmd.value);
+          }
+          break;
+
+        case 'S':
+          targetAngle = cmd.value;
+          changeSteeringAngle(cmd.value);
+          sendFeedback();
+
+          if (DEBUG) {
+            Serial.print(cmd.cmd);
+            Serial.println(cmd.value);
+          }
+          break;
+        default:
+          break;
+      }
+
+      input = "";
+    } else {
+      input += c;
+    }
+  }
 }
