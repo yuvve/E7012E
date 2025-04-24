c7696985e49d02ae8f2daa1e50d377701447a1c0
yuvve <37124628+yuvve@users.noreply.github.com>
2025-04-24 12:14:50 +0200

    Possible fix for infinity speed

--- labs/lab-3/sensors.ino
+++ labs/lab-3/sensors.ino
@@ -5,7 +5,7 @@ SensorData sensorData;
 SpeedSensor speedSensor;
 
 void setupSensorData() {
-  sensorData.speed = 0;
+  sensorData.speed = 0.0f;
 }
 
 void setupSpeedSensor(unsigned int pin) {
@@ -21,7 +21,7 @@ void speedSensorISR() {
   speedSensor.pulses++;
   if (speedSensor.pulses % 4) {
     speedSensor.t0 = speedSensor.t1;
-    speedSensor.t1 = micros()/1000000;
+    speedSensor.t1 = (int) (micros()/1000000);
     setSpeed(CALC_SPEED(speedSensor.t0, speedSensor.t1, (WHEEL_DIAM_CM/2)*100));
   }
 }
