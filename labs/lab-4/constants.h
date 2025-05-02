fceb5da868425a407435022bfb68b220298a169b
Simon Larsson <99037566+simonlarsson517@users.noreply.github.com>
2025-05-02 10:35:05 +0200

    Fix debug serial macro

--- labs/lab-4/constants.h
+++ labs/lab-4/constants.h
@@ -3,10 +3,10 @@
   // Global settings
   #define DEBUG (1)
   #if DEBUG
-    #define DEBUG_PRINT(x) Serial.println(x)
+    #define DEBUG_PRINT(x) Serial.print(x)
     #define DEBUG_PRINTLN(x) Serial.println(x)
     #define DEBUG_PRINTF(fmt, ...) { \
-      char buffer[64]; /* Adjust buffer size as needed */ \
+      char buffer[64]; \
       snprintf(buffer, sizeof(buffer), fmt, __VA_ARGS__); \
       Serial.println(buffer); \
   }
