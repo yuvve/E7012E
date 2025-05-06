9c795bfa548db5977f90aa5c07a2d59e728e6fe4
yuvve <37124628+yuvve@users.noreply.github.com>
2025-05-06 14:24:55 +0200

    Remove mixed variables to prevent UB

--- labs/lab-4/motor.ino
+++ labs/lab-4/motor.ino
@@ -13,12 +13,12 @@ void setupMotor(int pin) {
 * Anything above zero maps to a mapping function that starts at around 24%, 
 * as anything under that is not enough to make the motor move.
 **/
-void setTargetMotorRPMPercent(int speedPercent) {
-  if (speedPercent == 0) {
+void setTargetMotorRPMPercent(float speedPercent) {
+  if (speedPercent <= 0.001) {
     motor.writeMicroseconds(1500);
     return;
   }
-  int correctedValue = map(speedPercent, 0, 100, 1600, 2000);
+  int correctedValue = map((int) (100*speedPercent), 0, 100*100, 1600, 2000);
   correctedValue = constrain(correctedValue, 1500, 2000);
   motor.writeMicroseconds(correctedValue);
 }
