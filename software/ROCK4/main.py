34674dc7023b598b2c450a79942672b0d90a72db
filthytv <filthytv@ludd.ltu.se>
2025-05-09 14:17:15 +0200

    save_data

--- software/ROCK4/main.py
+++ software/ROCK4/main.py
@@ -5,6 +5,18 @@ from serial_communication import start_receiver, send, close, vals, val_lock
 from input_handler import start_input_thread
 from camera_interface  import start_camera_thread, stop_camera, camera_val_lock
 from camera_detection import camera_vals
+import csv
+
+def save_data_csv(time, left, right, forward):
+    with open("sensor_data", 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        # write header
+        writer.writerow(['time', 'left', 'right', 'forward'])
+
+        # write each pair of values concurrently
+        rows = zip(time, left, right, forward)
+        writer.writerows(rows)
 
 def main():
 
@@ -15,11 +27,22 @@ def main():
 
     # Initialize video capture
     start_camera_thread()
+    right = []
+    left = []
+    forward = []
+    time = []
 
     try:
         while True:
             clear()
             display(vals, camera_vals, val_lock, camera_val_lock)
+
+            right.append(vals['right_distance'])
+            left.append(vals['left_distance'])
+            forward.append(vals['forward_distance'])
+
+            data_sampling_time =+ 0.1
+            time.append(data_sampling_time)
 
             with camera_val_lock:
                 if camera_vals['color'] == "red":
@@ -46,6 +69,7 @@ def main():
     finally:
         send("M0")
         stop_camera()
+        save_data_csv(time, left, right, forward)
         close()
         print("Goodbye!")
 
