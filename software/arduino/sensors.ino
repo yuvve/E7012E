5f8ceed2f0afa106024263ddb385b5158709b933
yuvve <37124628+yuvve@users.noreply.github.com>
2025-05-12 13:17:43 +0200

    Lab 4 prox pid (#13)
    * Add single proximity sensor trigger/echo
    
    * Rename proximity sensor data structure
    
    * Add debug print macros
    
    * Add left/right proximity sensor handling
    
    * Fix debug serial macro
    
    * Refactor proximity
    
    * Add steering PID
    
    * Remove data from serial feedback
    
    * Use both sensors to determine center offset
    
    * Add forward proximity sensor and adjust steering constraints
    
    * Prevent crashing into front walls
    
    * Fix anti-crash distance
    
    * Dont adjust servo when motor is off
    
    * Remove mixed variables to prevent UB
    
    * Adjust servo constraints
    
    * Update default constants for distance PID
    
    * Increase proximity frequency, add constants
    
    * Remove implicit float->int conversions
    
    * Fix for 0 angle
    
    * Add yaml file to use LSP outside Arduino IDE
    
    * Calibrate steering constants
    
    * Allow lowercase letters in commands
    
    * Change folder structure
    
    * Change folder structure
    
    ---------
    
    Co-authored-by: Simon Larsson <99037566+simonlarsson517@users.noreply.github.com>

--- software/arduino/sensors.ino
+++ software/arduino/sensors.ino
@@ -4,16 +4,14 @@ SensorData sensorData;
 SpeedSensor speedSensor;
 
 void setupSensorData() {
-  sensorData = {0.0f};
+  sensorData.speed = 0.0f;
 }
 
 void setupSpeedSensor(unsigned int pin) {
   pinMode(pin, INPUT);
   speedSensor = {micros(), 0, 0};
   attachInterrupt(digitalPinToInterrupt(pin), speedSensorISR, RISING); 
-  if DEBUG {
-    Serial.println("Speed sensor initialized");
-  }
+  DEBUG_PRINTLN("Speed sensor initialized");
 }
 
 void speedSensorISR() {
@@ -38,4 +36,102 @@ float getSpeed() {
   float speed = sensorData.speed;
   interrupts();
   return speed;
+}
+
+void setupProximitySensor(uint echoPin, uint triggerPin, ProximitySensor& sensor, void (*isr)()) {
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(echoPin), isr, CHANGE);
+
+  sensor.triggerData = {triggerPin, false, 0};
+  // Volatile fields cant be brace-initialized, so we need to do it field by field
+  sensor.echoData.echoPin = echoPin;
+  sensor.echoData.tEchoStart = 0;
+  sensor.echoData.tEchoEnd = 0;
+  sensor.echoData.range = 0.0f;
+
+  DEBUG_PRINTF("Proximity sensor initialized with trigger pin: %d, echo pin: %d", triggerPin, echoPin);
+}
+
+// Arduino API does not support detecting the interrupt pin, so we need to use
+// sepparate ISR for each pin
+void leftProximityISR() {
+  handleProximityISR(leftProximitySensor);
+}
+
+// Arduino API does not support detecting the interrupt pin, so we need to use
+// sepparate ISR for each pin
+void rightProximityISR() {
+  handleProximityISR(rightProximitySensor);
+}
+
+// Arduino API does not support detecting the interrupt pin, so we need to use
+// sepparate ISR for each pin
+void forwardProximityISR() {
+  handleProximityISR(forwardProximitySensor);
+}
+
+/**
+  * Handle the proximity sensor interrupt. The function will read the 
+  * rising and falling edge of the echo pin and calculate the range.
+ */
+void handleProximityISR(ProximitySensor& sensor) {
+  noInterrupts(); 
+  uint now = micros();
+  if (digitalRead(sensor.echoData.echoPin)) {  // Rising edge
+    sensor.echoData.tEchoStart = now;    
+  } else { // Falling edge
+    sensor.echoData.tEchoEnd = now;
+    uint width = now - sensor.echoData.tEchoStart;
+    float range = width * SPEED_OF_SOUND_CM_US/2;
+    sensor.echoData.range = range;
+  }
+  interrupts();
+}
+
+/**
+  * Trigger the proximity sensor by sending a pulse to the trigger pin.
+  * The function will return true when the trigger is complete. 
+  * The function will alternate between left and righ sensors, 
+  * to prevent interference.
+**/
+bool triggerProximitySensor() {
+  ProximitySensor* sensor;
+  switch (triggerState) {
+    case TRIGGER_LEFT:
+      sensor = &leftProximitySensor;
+      triggerState = TRIGGER_RIGHT;
+      break;
+    case TRIGGER_RIGHT:
+      sensor = &rightProximitySensor;
+      triggerState = TRIGGER_FORWARD;
+      break;
+    case TRIGGER_FORWARD:
+      sensor = &forwardProximitySensor;
+      triggerState = TRIGGER_LEFT;
+      break;
+  }
+
+  uint now = micros();
+
+  if (!sensor->triggerData.isTriggered) {
+    digitalWrite(sensor->triggerData.triggerPin, HIGH);
+    sensor->triggerData.tTriggered = now;
+    sensor->triggerData.isTriggered = true;
+  } 
+  else if (now - sensor->triggerData.tTriggered >= PROXIMITY_PULSE_DURATION_US) {
+    digitalWrite(sensor->triggerData.triggerPin, LOW);
+    sensor->triggerData.isTriggered = false;
+
+    return true;
+  }
+
+  return false; 
+}
+
+float getProximityRange(ProximitySensor& sensor) {
+  noInterrupts();
+  float range = sensor.echoData.range;
+  interrupts();
+  return range;
 }
