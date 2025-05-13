3e370fc3d7aac49299c64883daa8fedb8d2b6be7
filthytv <filthytv@ludd.ltu.se>
2025-05-13 09:45:21 +0200

    interface changes

--- software/ROCK4/main.py
+++ software/ROCK4/main.py
@@ -1,11 +1,12 @@
+import csv
 import queue
 
+from camera_detection import camera_vals
+from camera_interface import stop_camera, camera_val_lock
+from input_handler import start_input_thread
 from interface import clear, display
 from serial_communication import start_receiver, send, close, vals, val_lock
-from input_handler import start_input_thread
-from camera_interface  import start_camera_thread, stop_camera, camera_val_lock
-from camera_detection import camera_vals
-import csv
+
 
 def save_data_csv(time, left, right, forward):
     with open("sensor_data.csv", 'w', newline='') as csvfile:
@@ -18,13 +19,17 @@ def save_data_csv(time, left, right, forward):
         rows = zip(time, left, right, forward)
         writer.writerows(rows)
 
-def main():
 
+def main():
     # Initialize serial communication
     cmd_q = queue.Queue()
     start_receiver()
     start_input_thread(cmd_q)
+    cmd = ""
+    fast = {"P": "-", "I": "-", "D": "-"}
+    slow = {"p": "-", "i": "-", "d": "-"}
 
+    '''
     # Initialize video capture
     start_camera_thread()
     right = []
@@ -32,22 +37,25 @@ def main():
     forward = []
     time = []
     data_sampling_time = 0
+    '''
 
     try:
         while True:
             clear()
-            display(vals, camera_vals, val_lock, camera_val_lock)
+            display(vals, camera_vals, val_lock, camera_val_lock, cmd, fast, slow)
 
+            '''
             right.append(vals['right_distance'])
             left.append(vals['left_distance'])
             forward.append(vals['forward_distance'])
 
             time.append(data_sampling_time)
             data_sampling_time = data_sampling_time + 0.1
+            '''
 
             with camera_val_lock:
                 if camera_vals['color'] == "red":
-                  send("M0")
+                    send("M0")
 
                 if camera_vals['color'] == "green":
                     if camera_vals['shape'] == "circle":
@@ -61,17 +69,32 @@ def main():
             if cmd.lower() in "exit":
                 break
 
-            #print(cmd)
+            # print(cmd)
             send(cmd)
+
+            match cmd[0]:
+                case "P":
+                    fast["P"] = cmd
+                case "I":
+                    fast["I"] = cmd
+                case "D":
+                    fast["D"] = cmd
+                case "p":
+                    slow["p"] = cmd
+                case "i":
+                    slow["i"] = cmd
+                case "d":
+                    slow["d"] = cmd
 
     except KeyboardInterrupt:
         pass
-    
+
     finally:
         send("M0")
         stop_camera()
-        save_data_csv(time, left, right, forward)
+        # save_data_csv(time, left, right, forward)
         close()
         print("Goodbye!")
+
 
 main()
