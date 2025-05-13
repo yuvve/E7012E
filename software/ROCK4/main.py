f69a8ee93d9a387917c85f1bd3dde4b7a22ebfb8
yuvve <37124628+yuvve@users.noreply.github.com>
2025-05-13 09:53:16 +0200

    Interface changes (#18)
    * added distance to interface
    
    * fix for serial monitor
    
    * start/stop
    
    * red/green?
    
    * fix forward serial prox
    
    * fix start
    
    * attempt 2
    
    * attempt 3
    
    * save_data
    
    * fix .csv and time constant
    
    * interface changes
    
    ---------
    
    Co-authored-by: filthytv <filthytv@ludd.ltu.se>

--- software/ROCK4/main.py
+++ software/ROCK4/main.py
@@ -1,19 +1,19 @@
+import csv
 import queue
 
+from camera_detection import camera_vals
+from camera_interface import stop_camera, camera_val_lock
+from input_handler import start_input_thread
 from interface import clear, display
 from serial_communication import start_receiver, send, close, vals, val_lock
-from input_handler import start_input_thread
-from camera_interface import start_camera_thread, stop_camera, camera_val_lock
-from camera_detection import camera_vals
-import csv
 
 
 def save_data_csv(time, left, right, forward):
-    with open("sensor_data.csv", "w", newline="") as csvfile:
+    with open("sensor_data.csv", 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
         # write header
-        writer.writerow(["time", "left", "right", "forward"])
+        writer.writerow(['time', 'left', 'right', 'forward'])
 
         # write each pair of values concurrently
         rows = zip(time, left, right, forward)
@@ -21,11 +21,16 @@ def save_data_csv(time, left, right, forward):
 
 
 def main():
+
     # Initialize serial communication
     cmd_q = queue.Queue()
     start_receiver()
     start_input_thread(cmd_q)
+    cmd = ""
+    fast = {"P": "-", "I": "-", "D": "-"}
+    slow = {"p": "-", "i": "-", "d": "-"}
 
+    '''
     # Initialize video capture
     start_camera_thread()
     right = []
@@ -33,11 +38,29 @@ def main():
     forward = []
     time = []
     data_sampling_time = 0
+    '''
 
     try:
         while True:
             clear()
-            display(vals, camera_vals, val_lock, camera_val_lock)
+            display(vals, camera_vals, val_lock, camera_val_lock, cmd, fast, slow)
+
+            '''
+            right.append(vals['right_distance'])
+            left.append(vals['left_distance'])
+            forward.append(vals['forward_distance'])
+
+            time.append(data_sampling_time)
+            data_sampling_time = data_sampling_time + 0.1
+            '''
+
+            with camera_val_lock:
+                if camera_vals['color'] == "red":
+                    send("M0")
+
+                if camera_vals['color'] == "green":
+                    if camera_vals['shape'] == "circle":
+                        send("M0.5")
 
             right.append(vals["right_distance"])
             left.append(vals["left_distance"])
@@ -64,13 +87,27 @@ def main():
             # print(cmd)
             send(cmd)
 
+            match cmd[0]:
+                case "P":
+                    fast["P"] = cmd
+                case "I":
+                    fast["I"] = cmd
+                case "D":
+                    fast["D"] = cmd
+                case "p":
+                    slow["p"] = cmd
+                case "i":
+                    slow["i"] = cmd
+                case "d":
+                    slow["d"] = cmd
+
     except KeyboardInterrupt:
         pass
 
     finally:
         send("M0")
         stop_camera()
-        save_data_csv(time, left, right, forward)
+        # save_data_csv(time, left, right, forward)
         close()
         print("Goodbye!")
 
